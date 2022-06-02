8f8a71fa1c9d0c60b5d52ef9d545c6d0962f6aa9
Nikolaj Majorov <nikolaj@majorov.biz>
2022-06-02 12:18:25 +0200

    add initia registration

--- Services/AppAuthenticationStateProvider.cs
+++ Services/AppAuthenticationStateProvider.cs
@@ -4,17 +4,17 @@ namespace BlazorAppETF.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using System.Security.Claims;
 
-public class AppAuthenticationStateProvider :AuthenticationStateProvider
+public class AppAuthenticationStateProvider : AuthenticationStateProvider
 {
 
-     
+
     private readonly IDataService _dataService;
     private readonly AccountService _accountService;
 
-    public AppAuthenticationStateProvider(IDataService dataService,AccountService accountService)
+    public AppAuthenticationStateProvider(IDataService dataService, AccountService accountService)
     {
         _dataService = dataService;
-        _accountService =accountService;
+        _accountService = accountService;
     }
 
 
@@ -24,10 +24,9 @@ public class AppAuthenticationStateProvider :AuthenticationStateProvider
 
         var currentUser = _dataService.CurrentUser;
         if (currentUser == null || !currentUser.IsAuthenticated)
-        { 
-            //TODO implement
-        }       
-        if (currentUser.IsAuthenticated)
+        {
+            //TODO implement?
+        }else if    <(currentUser.IsAuthenticated)
         {
             var claims = new[] { new Claim(ClaimTypes.Name, _dataService.CurrentUser.UserName) }.Concat(_dataService.CurrentUser.Claims.Select(c => new Claim(c.Key, c.Value)));
             identity = new ClaimsIdentity(claims, "Server authentication");
@@ -37,16 +36,21 @@ public class AppAuthenticationStateProvider :AuthenticationStateProvider
     }
 
 
-    
+
     public async Task Login(User login)
     {
-        
-            await _accountService.Login(login);
-            //_dataService.CurrentUser = login;
 
-            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
-        
+        await _accountService.Login(login);
+        _dataService.CurrentUser = login;
+
+        NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+
     }
-    
+
+    public async Task Register(User login)
+    {
+        await _accountService.Register(login);
+        NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+    }
 }
 
